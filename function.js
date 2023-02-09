b04eb91d12455211c6ea080274250a106ad61512
EricaSubelza <124851446+erisubelza@users.noreply.github.com>
2023-02-09 16:52:11 -0300

    df

--- function.js
+++
@@ -1,6 +0,0 @@
-
-
-document.getElementById('button-1').onclick=function () { 
-    console.log("Capturamos el evento click");
-    document.getElementById("demo").innerHTML = "No hay proyectos recientes"
-}
